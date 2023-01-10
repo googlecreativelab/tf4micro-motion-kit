dfb963c26afeaabfc152439aa7cc91ef40c42a46
Ryan Rubush <rrubush@gmail.com>
2023-01-10 12:04:24 -0500

    Update serve.sh

--- web/serve.sh
+++ web/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# if you want to run live server for watching and auto-reloading, isntall with
+# if you want to run live server for watching and auto-reloading, install with
 # $npm install -g live-server
 PORT=9090
 live-server --port=$PORT || python3 -m http.server $PORT || python -m SimpleHTTPServer $PORT
